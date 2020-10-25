f415477dd89cbb5076901e5002d9623694c53394
benitezfernando <benitezfernando87@gmail.com>
2020-10-25 16:00:06 -0300

    featured: add components and custom hooks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useState, useEffect} from 'react';
 import Header from './components/Header';
 import Formulario from './components/Formulario';
+import ListadoNoticias from './components/ListadoNoticias';
 
 
 function App() {
+  //Definir la categoria y noticias
+  const [categoria, guardarCategoria] = useState('');
+  const [noticias, guardarNoticias] = useState([]);
+
+  useEffect(() => {
+    const consultarApi = async() => {
+        const url = `http://newsapi.org/v2/top-headlines?country=ar&category=${categoria}&apiKey=f589c5ef17684b3286340ffa45975d18`;
+
+        const repuesta = await fetch (url);
+        const noticias = await repuesta.json();
+
+        guardarNoticias(noticias.articles);
+
+
+
+    }
+    consultarApi();
+  }, [categoria]);
+
+
+
   return (
     <Fragment>
       <Header
@@ -11,7 +33,13 @@ function App() {
       />
 
       <div className='container white'>
-        <Formulario/>
+        <Formulario
+          guardarCategoria={guardarCategoria}
+        />
+
+        <ListadoNoticias
+            noticias={noticias}
+        />
 
       </div>
     </Fragment>
--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,13 +1,45 @@
 import React from 'react';
 import styles from './Formulario.module.css';
+import PropTypes from 'prop-types';
+import useSelect from '../hooks/useSelect';
 
 
-const Formulario = () => {
+const Formulario = ({guardarCategoria}) => {
+
+    const OPCIONES = [
+        {value: 'general' , label: 'General'},
+        {value: 'business' , label: 'Negocios'},
+        {value: 'entertainment' , label: 'Entretenimiento'},
+        {value: 'health' , label: 'Salud'},
+        {value: 'science' , label: 'Ciencia'},
+        {value: 'sports' , label: 'Deportes'},
+        {value: 'technology' , label: 'Tecnologia'},
+        
+    ]
+
+
+
+    //utilizar custom hooks
+    const [categoria, SelectNoticias] = useSelect('general', OPCIONES);
+
+    //submit al form, pasar categoria a app.js
+    const buscarNoticias = e => {
+        e.preventDefault();
+
+        guardarCategoria(categoria);
+    }
+
+
     return ( 
         <div className={`${styles.buscador} row`}>
             <div className='col s12 m8 offset-m2'>
-                <form>
+                <form
+                    onSubmit={buscarNoticias}
+                >
                     <h2 className={styles.heading}>Encuentra noticias por categoria</h2>
+
+                        <SelectNoticias/>
+
                     <div className='input-field col s12'>
                         <input
                             type='submit'
@@ -22,6 +54,10 @@ const Formulario = () => {
 
         </div>
      );
+}
+
+Formulario.propTypes = {
+    guardarCategoria: PropTypes.func.isRequired
 }
  
 export default Formulario;
